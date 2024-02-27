ced628cd09c63cbed9dd2b2fc0487fcd43ea4623
AmazingAnt8 <anwyntanner@gmail.com>
2024-02-26 23:33:09 -0700

    login

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ function newUser () {
     localStorage.setItem("username", username.value);
     const password = document.querySelector("#password");
     localStorage.setItem("password", password.value);
-    //store this somewhere
+    window.location.href = "personalize.html";
+}
+
+function login () {
+    if (document.querySelector("#username") == localStorage.getItem("username")) /*why isn't this working?*/{
+        window.location.href = "quandary_selection.html";
+    }
+    else {
+        alert("Must enter valid username and password");
+    }
 }
