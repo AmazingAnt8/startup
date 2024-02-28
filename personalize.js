e0e050cff44c64763a974b7391064b52a9400314
AmazingAnt8 <anwyntanner@gmail.com>
2024-02-28 09:49:26 -0700

    quandary functions

--- personalize.js
+++ personalize.js
@@ -1,7 +1,12 @@
+var newQuandary = document.getElementById("new_quandary")
+var quandarySave = document.getElementById("quandary_save")
+quandarySave.addEventListener("click", function addNewQuandary() {
+    var data = {
+        "item": newQuandary.value,
+        "numOfSuggestions": 0
+    }
+})
+
 function addNewSuggestion() {
 
-}
-
-function addNewQuandary() {
-    
 }
