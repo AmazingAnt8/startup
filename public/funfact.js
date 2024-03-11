76e934f72f4021ccf5a1f2e4f508eae65cc96451
AmazingAnt8 <anwyntanner@gmail.com>
2024-03-11 11:31:34 -0600

    3rd party api

--- public/funfact.js
+++ public/funfact.js
@@ -1,21 +1,21 @@
-const axios = require('axios');
 
-function displayFact() {
-    const options = {
-        method: 'GET',
-        url: 'https://numbersapi.p.rapidapi.com/8/math',
-        params: {json: 'true'},
-        headers: {
-          'X-RapidAPI-Key': '8abd71fb1amsh455b57763382a28p1dea2djsn7d25bcae95ba',
-          'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-        }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          console.log(response.data);
-      } catch (error) {
-          console.error(error);
-      }
-}
-displayFact();
+
+// function displayQuote(data) {
+//     fetch('https://api.quotable.io/random')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const containerEl = document.querySelector('#quote');
+  
+//         const quoteEl = document.createElement('p');
+//         quoteEl.classList.add('quote');
+//         const authorEl = document.createElement('p');
+//         authorEl.classList.add('author');
+  
+//         quoteEl.textContent = data.content;
+//         authorEl.textContent = data.author;
+  
+//         containerEl.appendChild(quoteEl);
+//         containerEl.appendChild(authorEl);
+//       });
+// }
+// displayQuote();
