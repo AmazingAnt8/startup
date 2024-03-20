e9799b45e30c6b76a279379aff3b5d384dd39ad1
AmazingAnt8 <anwyntanner@gmail.com>
2024-03-20 15:40:44 -0600

    mongodb

--- public/funfact.js
+++ public/funfact.js
@@ -1,24 +1,26 @@
-// async function displayFact () {
-//     const url = 'https://numbersapi.p.rapidapi.com/1729/math?fragment=true&json=true';
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '8abd71fb1amsh455b57763382a28p1dea2djsn7d25bcae95ba',
-//             'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-//         }
-//     };
+async function displayFact () {
+    const url = 'https://numbersapi.p.rapidapi.com/1729/math?fragment=true&json=true';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '8abd71fb1amsh455b57763382a28p1dea2djsn7d25bcae95ba',
+            'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+        }
+    };
 
-//     try {
-//         const response = await fetch(url, options);
-//         const result = await response.text();
-//         console.log(result);
-//     } catch (error) {
-//         console.error(error);
-//     }
-//         localStorage.setItem('fun_fact', result);
-//     }
-
-// document.getElementById("fact").innerHTML = localStorage.getItem('fun_fact');
+    try {
+        const response = await fetch(url, options);
+        const result = await JSON.stringify(response["text"]);
+        console.log(typeof response);
+        console.log(result);
+        localStorage.setItem('fun_fact', result);
+    } catch (error) {
+        console.error(error);
+    }
+       
+}
+displayFact();
+document.getElementById("fact").innerHTML = localStorage.getItem('fun_fact');
 
 
 // function displayFact(data) {
@@ -32,22 +34,22 @@
 // displayFact();
 
 
-function displayQuote(data) {
-    fetch('https://api.quotable.io/random')
-      .then((response) => response.json())
-      .then((data) => {
-        const containerEl = document.querySelector('#quote');
+// function displayQuote(data) {
+//     fetch('https://api.quotable.io/random')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const containerEl = document.querySelector('#quote');
   
-        const quoteEl = document.createElement('p');
-        quoteEl.classList.add('quote');
-        const authorEl = document.createElement('p');
-        authorEl.classList.add('author');
+//         const quoteEl = document.createElement('p');
+//         quoteEl.classList.add('quote');
+//         const authorEl = document.createElement('p');
+//         authorEl.classList.add('author');
   
-        quoteEl.textContent = data.content;
-        authorEl.textContent = data.author;
+//         quoteEl.textContent = data.content;
+//         authorEl.textContent = data.author;
   
-        containerEl.appendChild(quoteEl);
-        containerEl.appendChild(authorEl);
-      });
-}
-displayQuote();
+//         containerEl.appendChild(quoteEl);
+//         containerEl.appendChild(authorEl);
+//       });
+// }
+// displayQuote();
